cc1e02a486ccad00404b879e322e30d9ba896b46
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-09-25 22:14:50 +0700

    [UPDATE] import Nabar and Bg

---
+++ src/component/Core/Background.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useSpring, animated } from 'react-spring';
+import '../../style.css' // // Icons made by Freepik from www.flaticon.com
+
+const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2]
+const trans1 = (x, y) => `translate3d(${x / 10}px,${y / 10}px,0)`
+const trans2 = (x, y) => `translate3d(${x / 8}px,${y / 8 - 100}px,0)`
+
+
+const Bg = styled.div`
+  background-color: #2E3445;
+  height: 100;
+  z-index:-100;
+  `
+
+const Container = styled.div`
+  align-items: center;
+  justify-content: center;
+`
+
+
+
+function Card() {
+  const [props, set] = useSpring(() => ({ xy: [0, 0], config: { mass: 10, tension: 600, friction: 140 } }))
+  return (
+    <Bg>
+    <Container  onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}>
+      <animated.div class="card1" style={{ transform: props.xy.interpolate(trans1) }} />
+      <animated.div class="card2" style={{ transform: props.xy.interpolate(trans2) }} />
+    </Container>
+    </Bg>
+  )
+}
+
+export default Card;
---
+++ src/component/Core/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Nav = styled.div`
+  background-color: #525E65;
+  height: 8vh;
+  z-index: 1;
+`
+
+const Navbar = () =>{
+  return (
+    <Nav/>
+  )
+}
+
+export default Navbar;

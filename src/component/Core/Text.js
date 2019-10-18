d2985c5d2404c8a07aae0790b9611e3323309a57
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-10-19 01:07:32 +0700

    update add title and subtitle

--- src/component/Core/Text.js
+++ src/component/Core/Text.js
@@ -6,4 +6,17 @@ export const Hello = styled.p`
   font-size: ${fontsize.hello};
   color: ${color.font};
   line-height:0px;  
+  text-align: right;
+`
+
+export const Title = styled.p`
+  font-size: ${fontsize.title};
+  color: ${color.font};
+  line-height:0px;  
+`
+
+export const Subtitle = styled.p`
+  font-size: ${fontsize.subtitle};
+  color: ${color.font};
+  line-height:0px;  
 `

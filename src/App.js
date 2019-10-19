6666bae58bb74a98a508c9f9483b318951fcb961
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-10-19 18:48:17 +0700

    update  add Container and Grid

--- src/App.js
+++ src/App.js
@@ -3,14 +3,28 @@ import './App.css';
 import Nav from './component/Core/Navbar';
 import Bg from './component/Core/Background';
 import Home from './component/Home'
+import styled from 'styled-components'
+import { Container,Grid } from '@material-ui/core'
+
+const BgHome = styled(Grid)`
+  height: 100vh;
+  z-index:0;
+  margin-top: 12%;
+  `
 
 function App() {
   return (
-    <div>
+    <React.Fragment>
       <Nav/>
-      <Home/>
-      <Bg/>
-    </div>
+      <Container maxWidth="lg">
+        <Grid lg={12}>
+          <BgHome  justify="center" alignItems="center">
+             <Home/>
+          </BgHome>
+          <Bg/>
+        </Grid>
+      </Container>
+    </React.Fragment>
   );
 }
 

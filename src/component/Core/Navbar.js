5e4f9fa337ae2032daed049e3b3f18c2ac2542a7
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-10-21 13:32:18 +0700

    update navbar

--- src/component/Core/Navbar.js
+++ src/component/Core/Navbar.js
@@ -1,15 +1,54 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Menu } from '@material-ui/core';
 
-const Nav = styled.div`
+const Nav = styled.nav`
   background-color: #525E65;
   height: 8vh;
-  z-index: 1;
+  z-index: 999 !important;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  ul {
+    text-align:center;
+    padding: 0;
+  }
+  li {
+    list-style-type: none;
+    display: inline-block;
+  }
+  li a {
+    text-decoration: none;
+    display: block;
+    color: white;
+    text-align: center;
+    width: 150px;
+    transition: 0.4s;
+  }
+  li a:hover {
+    cursor: pointer;
+    color: #72C9A6;
+  }
 `
 
 const Navbar = () =>{
   return (
-    <Nav/>
+    <Nav>
+      <ul>
+        <li>
+          <a href="home">Home</a>
+        </li>
+        <li>
+          <a href="profile">Profile</a>
+        </li>
+        <li>
+          <a href="work">Work Experience</a>
+        </li>
+        <li>
+          <a href="skills">Skills</a>
+        </li>
+      </ul>
+    </Nav>
   )
 }
 

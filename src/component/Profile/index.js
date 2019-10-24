b957aeae266405a763bb8d6590646be57c66a833
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-10-24 19:01:28 +0700

    Update test content of profile

---
+++ src/component/Profile/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class index extends Component {
+  render() {
+    return (
+      <div >
+        Hello Test.
+      </div>
+    );
+  }
+}
+
+export default index;

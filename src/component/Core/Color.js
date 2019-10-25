2fd45d08e28313001b51c6091e939e0626049c47
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-10-25 23:49:12 +0700

    [Update] color.js

--- src/component/Core/Color.js
+++ src/component/Core/Color.js
@@ -2,7 +2,7 @@ export default {
   primary: '#2E3445',
   bg: '#2E3445',
   bgProfile: '#393E4E',
-  bgSills:'#424857',
+  bgSkills:'#424857',
   bgWork:'#4C515F',
   secondary: '#525E65',
   green:'#72C9A6',

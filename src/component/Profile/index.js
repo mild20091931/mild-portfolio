6f3f2dddbcf070a4acbabbeb627a394a06c94929
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-10-25 18:16:11 +0700

    [Update] add interests

--- src/component/Profile/index.js
+++ src/component/Profile/index.js
@@ -2,19 +2,26 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Header,Title,Subtitle } from '../Core/Text';
 import { Grid,Container,Button, Box } from '@material-ui/core'
-import {TagButton} from '../Core/Button'
 
 const contact = [
+  {icon : "e-mail",text : "tassaneewan.mild@mail.kmutt.ac.th" ,link: "https://github.com/mild20091931"},
   {icon : "github",text : "github.com/mild20091931" ,link: "https://github.com/mild20091931"},
   {icon : "facebook",text: "facebook.com/mmmild1",link: "https://www.facebook.com/mmmild1"},
   {icon : "medium",text : "medium.com/@mild20091931",link: "https://medium.com/@mild20091931"},
   {icon : "phone",text: "0956787294",link: "-"},
 ]
+
+const Interests = [{
+  text: "Web Technology",
+  text: "Frontend Technology",
+  text: "Tool for developer",
+  text: "linux"
+}]
 class index extends Component {
   render() {
     return (
     <React.Fragment>
-      <Container maxWidth="md">
+      <Container maxWidth="md" >
         <Grid item md="12">
           <Title>Profile</Title>
           <Subtitle>My name is Tassneewan Noita. My nick name is Mild. I was born 2nd September 1997. I'm 22 year old.</Subtitle>
@@ -24,14 +31,24 @@ class index extends Component {
           <Subtitle>Bachelor of Science (Information Technology), School of Information Technology</Subtitle>
           <Subtitle>King Mongkut’s University of Technology Thonburi (3nd Year)</Subtitle>
         </Grid>
+        <Grid item md="12">
+          <Title>Interests</Title>
+          <Button variant="outlined" color="white">
+            Disabled
+          </Button>
+        </Grid>
         <Grid>
           <Title>Contact</Title>
-          <Grid container direction="row" item md="12">
-            <Box>
+          <Grid container direction="row" justify="center" item md="12">
+          {contact.map((data,i) => {
+            return (
+            <Box m={2} key={i}>
               <Button variant="contained" size="medium" >
-                github
+                {data.icon}
               </Button>
              </Box>
+            )
+          })}
           </Grid>
         </Grid>
       </Container>
